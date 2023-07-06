fd25fc323f0f570faf00c1f539e18b4b0028688c
DiogoHSSantos <115512182+DiogoHSSantos@users.noreply.github.com>
2023-07-06 10:47:56 -0300

    terminei o projeto

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,3 +9,24 @@ botaoAdiciona.addEventListener('click', () => {
 ulLista.addEventListener('mousedown', (botaoExcluir) => {
     novoItem.removerItem(botaoExcluir, ulLista);
 });
+// ------------------------------------------------------------
+const areaInserirNome = document.querySelector('.insertName');
+const inputNomeDaLista = document.querySelector('#ListName');
+const botaoCriaNomeLista = document.querySelector('.criarNome');
+const nomeDaLista = document.querySelector('.ListNameHeader');
+const botaoEditaNomeLista = document.querySelector('.bxs-pencil');
+botaoCriaNomeLista.addEventListener('click', () => {
+    nomeDaLista.textContent = inputNomeDaLista.value;
+    inputNomeDaLista.value = '';
+    areaInserirNome.style.transition = '.3s';
+    areaInserirNome.style.opacity = '0';
+    setTimeout(() => {
+        areaInserirNome.style.display = 'none';
+    }, 300);
+});
+botaoEditaNomeLista.addEventListener('click', () => {
+    setTimeout(() => {
+        areaInserirNome.style.display = 'flex';
+    }, 300);
+    areaInserirNome.style.opacity = '1';
+});
--- ts/app.ts
+++ ts/app.ts
@@ -13,3 +13,30 @@ botaoAdiciona.addEventListener('click', ()=>{
 ulLista.addEventListener('mousedown', (botaoExcluir:Event) =>{
     novoItem.removerItem(botaoExcluir, ulLista)
 })
+
+// ------------------------------------------------------------
+
+const areaInserirNome:HTMLElement = document.querySelector('.insertName');
+const inputNomeDaLista:HTMLInputElement = document.querySelector('#ListName');
+const botaoCriaNomeLista:HTMLButtonElement = document.querySelector('.criarNome');
+const nomeDaLista:HTMLElement = document.querySelector('.ListNameHeader');
+const botaoEditaNomeLista:HTMLButtonElement = document.querySelector('.bxs-pencil');
+
+
+botaoCriaNomeLista.addEventListener('click', ()=>{
+    nomeDaLista.textContent = inputNomeDaLista.value;
+    inputNomeDaLista.value = ''
+
+    areaInserirNome.style.transition = '.3s';
+    areaInserirNome.style.opacity = '0';
+    setTimeout(()=>{
+        areaInserirNome.style.display = 'none';
+    }, 300)
+});
+
+botaoEditaNomeLista.addEventListener('click', ()=>{
+    setTimeout(()=>{
+        areaInserirNome.style.display = 'flex';
+    }, 300)
+    areaInserirNome.style.opacity = '1';
+});
